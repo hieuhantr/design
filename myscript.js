e3abb6de7687efebc60e77a6ba5887434bec91a2
hieuhantr <70181576+hieuhantr@users.noreply.github.com>
2021-06-16 19:33:49 -0700

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -22,3 +22,34 @@ ScrollReveal().reveal('.text', { delay: 100 });
 ScrollReveal().reveal('.fullcolumn', { delay: 200 });
 ScrollReveal().reveal('.columnone', { delay: 100 });
 ScrollReveal().reveal('.columntwo', { delay: 100 });
+
+/* Slideshow */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
